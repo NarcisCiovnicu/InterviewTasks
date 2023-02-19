b452ac11a702a5e1e37285d9677f770c806e090b
NarcisCiovnicu <79647965+NarcisCiovnicu@users.noreply.github.com>
2023-02-19 12:35:13 +0200

    Upload project Custom List to github

---
+++ CustomList/CustomList/CustomList.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+
+namespace CustomList
+{
+    public class CustomList<T> : IList<T>
+    {
+        private T[] _items = Array.Empty<T>();
+        private int _size = 0;
+
+        public int Capacity { get; private set; } = 0;
+        public int Count => _size;
+        public bool IsReadOnly => false;
+
+        public CustomList(int capacity = 0)
+        {
+            if (capacity > 0)
+            {
+                Capacity = capacity;
+                _items = new T[capacity];
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                ThrowIfIndexOutOfRange(index);
+                return _items[index];
+            }
+            set
+            {
+                ThrowIfIndexOutOfRange(index);
+                _items[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_size == Capacity)
+            {
+                Resize();
+            }
+            _items[_size] = item;
+            _size++;
+        }
+
+        public void Insert(int index, T item)
+        {
+            ThrowIfIndexOutOfRange(index);
+            if (_size == Capacity)
+            {
+                Resize();
+            }
+
+            for (int i = _size; i > index; i--)
+            {
+                _items[i] = _items[i - 1];
+            }
+
+            _items[index] = item;
+            _size++;
+        }
+
+        public void Clear()
+        {
+            _items = new T[Capacity];
+            _size = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return IndexOf(item) >= 0;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                T current = _items[i];
+                if (current == null && item == null || current != null && current.Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ThrowIfIndexOutOfRange(index);
+            for (int i = index; i < _size - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _size--;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            }
+            if (_size > array.Length - arrayIndex)
+            {
+                throw new ArgumentOutOfRangeException(nameof(array), "Destination array was not long enough. Check the destination index, length, and the array's lower bounds.");
+            }
+            for (int i = 0; i < _size; i++)
+            {
+                array[arrayIndex++] = _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in _items)
+            {
+                yield return item;
+            }
+        }
+
+        private void ThrowIfIndexOutOfRange(int index)
+        {
+            if (index > _size - 1 || index < 0)
+            {
+                throw new ArgumentOutOfRangeException($"Index out of range: {index}");
+            }
+        }
+
+        private void Resize()
+        {
+            if (Capacity == 0)
+            {
+                Capacity = 4;
+                _items = new T[Capacity];
+            }
+            else
+            {
+                T[] resized = new T[Capacity * 2];
+                for (int i = 0; i < Capacity; i++)
+                {
+                    resized[i] = _items[i];
+                }
+                _items = resized;
+                Capacity *= 2;
+            }
+        }
+    }
+}
---
+++ CustomList/CustomListTests/CustomListTests.cs
@@ -0,0 +1,171 @@
+﻿using CustomList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class CustomListTests
+    {
+
+        [TestMethod()]
+        public void Test_Constructor()
+        {
+            List<int> list = new();
+            CustomList<int> customList = new();
+            Assert.AreEqual(list.Capacity, customList.Capacity);
+
+            list = new(15);
+            customList = new(15);
+            Assert.AreEqual(list.Capacity, customList.Capacity);
+        }
+
+        [TestMethod()]
+        public void Test_AddInsert()
+        {
+            List<int> list = new();
+            CustomList<int> customList = new();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            Assert.AreEqual(list.Count, customList.Count);
+            Assert.AreEqual(list[0], customList[0]);
+            Assert.AreEqual(list[1], customList[1]);
+            Assert.AreEqual(list[2], customList[2]);
+
+            list.Insert(0, -1);
+            list.Insert(1, -2);
+            customList.Insert(0, -1);
+            customList.Insert(1, -2);
+            Assert.AreEqual(list.Count, customList.Count);
+            Assert.AreEqual(list[0], customList[0]);
+            Assert.AreEqual(list[1], customList[1]);
+            Assert.AreEqual(list[2], customList[2]);
+            Assert.AreEqual(list[3], customList[3]);
+            Assert.AreEqual(list[4], customList[4]);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => customList.Insert(5, 999));
+        }
+
+        [TestMethod()]
+        public void Test_GetAndSet()
+        {
+            List<int> list = new();
+            CustomList<int> customList = new();
+
+            list.Add(1);
+            customList.Add(1);
+            Assert.AreEqual(list[0], customList[0]);
+
+            list[0] = 5;
+            customList[0] = 5;
+            Assert.AreEqual(list[0], customList[0]);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => customList[-1]);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => customList[3]);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => customList[-1] = 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => customList[3] = 2);
+        }
+
+        [TestMethod()]
+        public void Test_Clear()
+        {
+            List<int> list = new();
+            CustomList<int> customList = new();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+
+            list.Clear();
+            customList.Clear();
+            Assert.AreEqual(list.Count, customList.Count);
+        }
+
+        [TestMethod()]
+        public void Test_Contains_IndexOf()
+        {
+            List<int?> list = new();
+            CustomList<int?> customList = new();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(null);
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(null);
+
+            Assert.AreEqual(list.Contains(2), customList.Contains(2));
+            Assert.AreEqual(list.Contains(null), customList.Contains(null));
+            Assert.AreEqual(list.Contains(6), customList.Contains(6));
+
+            list.Remove(null);
+            customList.Remove(null);
+            Assert.AreEqual(list.Contains(null), customList.Contains(null));
+
+            Assert.AreEqual(list.IndexOf(2), customList.IndexOf(2));
+            Assert.AreEqual(list.IndexOf(null), customList.IndexOf(null));
+            Assert.AreEqual(list.IndexOf(5), customList.IndexOf(5));
+        }
+
+        [TestMethod()]
+        public void Test_Remove()
+        {
+            List<int> list = new();
+            CustomList<int> customList = new();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+
+            Assert.AreEqual(list.Remove(2), customList.Remove(2));
+            Assert.AreEqual(list.Remove(3), customList.Remove(3));
+            Assert.AreEqual(list.Remove(4), customList.Remove(4));
+            Assert.AreEqual(list.Count, customList.Count); // 1
+
+            list.Add(2);
+            list.Add(3);
+            customList.Add(2);
+            customList.Add(3);
+            // count 3
+
+            list.RemoveAt(2);
+            customList.RemoveAt(2);
+            Assert.AreEqual(list.Count, customList.Count); // 2
+            Assert.AreEqual(list[0], customList[0]);
+            Assert.AreEqual(list[1], customList[1]);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => customList.RemoveAt(2));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => customList.RemoveAt(3));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => customList.RemoveAt(-1));
+        }
+
+        [TestMethod()]
+        public void Test_CopyTo()
+        {
+            List<int> list = new();
+            CustomList<int> customList = new();
+            list.Add(1);
+            list.Add(2);
+            customList.Add(1);
+            customList.Add(2);
+
+            int[] arr1 = new int[2];
+            int[] arr2 = new int[2];
+
+            list.CopyTo(arr1, 0);
+            customList.CopyTo(arr2, 0);
+
+            Assert.AreEqual(arr1[0], arr2[0]);
+            Assert.AreEqual(arr1[1], arr2[1]);
+        }
+    }
+}
